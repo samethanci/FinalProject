f8f0614f0622e5c696488aa47367b13859d68408
Samet HANCI <Samet HANCI@SAMET>
2022-12-24 14:03:18 +0300

    24/12/2022

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,7 +31,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 20)
+            if (DateTime.Now.Hour == 12)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime); 
             }
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -9,14 +12,47 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet] 
-        public List<Product> Get()
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
         {
-            return new List<Product>
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult Get()
+        {
+            var result = _productService.GetAll();
+            if (result.Success)
             {
-                new Product {ProductId = 1, ProductName = "Elma"},
-                new Product {ProductId = 2, ProductName = "Armut"},
-            };
+                //return Ok(result.Data);
+                return Ok(result);
+            }
+            return BadRequest(result);
+            //return BadRequest(result.Message);
+            //return result.Message;
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult Get(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Post(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success) 
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
